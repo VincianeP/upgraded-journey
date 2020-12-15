5118b5cdf41b5fc0d24e79de05703e80831eefa6
Vinciane Piveteau <vinciane.piveteau@ens-lyon.fr>
2020-12-15 15:40:14 +0000

    transdecoder fonctionnel, à commenter

--- transdecoder.sh
+++ transdecoder.sh
@@ -5,17 +5,17 @@ cd $data
 
 #Create a folder to store the output of TransDecoder
 mkdir -p transdecoder_data
+cd transdecoder_data
 
 # Launch Transdecoder
 # Work in 2 or 3 steps (the second is facultative)
 # Step 1: extract the long open reading frames (TransDecoder.LongOrfs)
+
+TransDecoder.LongOrfs -t $data/trinity_data/Trinity.fasta -S --gene_trans_map $data/trinity_data/Trinity.fasta.gene_trans_map -O $data/transdecoder_data
+
+
 # Step 2: blast to identify peptides with homology to known proteins (optionnal) 
-# Step 3:predict the likely coding regions (TransDecoder.Predict)
+# Step 3: predict the likely coding regions (TransDecoder.Predict)
 
-#Question: Pourquoi ne pas utiliser l'output de la première étape dans la 3ème? 
-#          Que fait la 3ème exactement? (puisque la première étape donne déjà les ORFs)
-
-TransDecoder.LongOrfs -t $data/trinity_data/Trinity.fasta -S --output_dir transdecoder_data
-
-TransDecoder.Predict -t $data/trinity_data/Trinity.fasta -S --output_dir transdecoder_data
+TransDecoder.Predict -t $data/trinity_data/Trinity.fasta --single_best_only -O $data/transdecoder_data
 
