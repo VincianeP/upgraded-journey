c450f14eae2e488b2fe5ed004ed9716627d4ba72
Vinciane Piveteau <vinciane.piveteau@ens-lyon.fr>
2020-12-19 20:35:43 +0000

    script fonctionnel

--- DESeq2.R
+++ DESeq2.R
@@ -17,10 +17,10 @@ names(files) <- samples$run
 #Create a table containing col1=gene, col2=transcript
 tx2gene <- read.table(file=paste(dir,"/trinity_data/Trinity.fasta.gene_trans_map", sep=""),
                       header=FALSE,sep="\t",col.names=c("geneid", "transcript_name"))[,c(2,1)]
-                      
+
 #tximport
 txi <- tximport(files, type="salmon",tx2gene=tx2gene)
-         
+
 #DE
 ddsTxi <- DESeqDataSetFromTximport(txi, colData=samples, design= ~ condition)
 
@@ -31,4 +31,56 @@ dds <- ddsTxi[keep,]
 #Decide which condition is the reference 
 dds$condition <- relevel(dds$condition, ref= "white")
 
+#Run DESeq
 dds <- DESeq(dds)
+resultsNames(dds)
+
+#Create a MA plot
+res <- results(dds)
+
+library(ggplot2)
+ggplot(data=as.data.frame(res), mapping=aes(x=log10(baseMean),y=log2FoldChange)) + 
+  geom_point() + theme_bw()
+
+#Create a MA_plot_shrinked and take into account the relation between fold change and baseMean
+resLFC<-lfcShrink(dds, coef = "condition_orange_vs_white", type = "apeglm")
+maplot <- ggplot(data = as.data.frame(resLFC), mapping=aes(x=log10(baseMean),
+                                                 y=log2FoldChange, 
+                                                 color=padj<0.05,
+                                                 size=padj<0.05, 
+                                                 shape=padj<0.05,
+                                                 alpha=padj<0.05,
+                                                 fill=padj<0.05,)) + 
+  geom_point() + 
+  scale_color_manual(values=c("#999999","#FF6600")) +
+  scale_size_manual(values=c(0.1,1)) +
+  scale_alpha_manual(values=c(0.5,1)) +
+  scale_shape_manual(values=c(21,21)) +
+  scale_fill_manual(values=c("#999999","#000000")) +
+  theme_bw() + theme(legend.position = 'none')
+
+#Volcano plot
+volcano_plot <- ggplot(data=as.data.frame(resLFC), mapping=aes(x=log2FoldChange,y=-log10(padj),
+                                               color=padj<1e-20, 
+                                               alpha=padj<1e-20)) + 
+  geom_point() + 
+  scale_color_manual(values=c("#000000", "#FF6600")) +
+  scale_alpha_manual(values=c(0.5, 1)) +
+  theme(legend.position = 'none') +
+  theme_bw()
+
+#Sortir les gènes différentiellement exprimés 
+resLFC[is.na(resLFC$pvalue),"pvalue"] <- 1
+resLFC[is.na(resLFC$padj),"padj"] <- 1
+top_DE_genes <- resLFC[resLFC$padj<1e-20 & abs(resLFC$log2FoldChange)>2,]
+
+top_DE_neg <- top_DE_genes[top_DE_genes$log2FoldChange<0,] #14 genes
+top_DE_pos <- top_DE_genes[top_DE_genes$log2FoldChange>0,] #16 genes
+
+top_sorted_DE_neg <- top_DE_neg[order(top_DE_neg$padj),]
+top_sorted_DE_pos <- top_DE_pos[order(top_DE_pos$padj),]
+
+
+#Plot PCA
+rld <- rlog(dds, blind=FALSE)
+plotPCA(rld, intgroup=c("condition"))
